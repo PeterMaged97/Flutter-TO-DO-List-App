60261a9d95c7c4ad4207b969f9d94df4bae20dec
Peter <petermounir97@gmail.com>
2020-12-01 15:48:03 +0200

    Added functionality to Checkboxes

--- lib/Components/task_tile.dart
+++ lib/Components/task_tile.dart
@@ -11,17 +11,32 @@ class TaskTile extends StatefulWidget {
 
 class _TaskTileState extends State<TaskTile> {
 
-  bool v = false;
+  bool isChecked = false;
+  void toggleCheckBox(bool newValue){
+    setState(() {
+      isChecked = newValue;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text(widget.taskLabel),
-      trailing: Checkbox(value: v,onChanged: (val){
-        setState(() {
-          v = val;
-        });
-      },tristate: false,),
+      title: Text(widget.taskLabel, style: TextStyle(decoration: isChecked ? TextDecoration.lineThrough : null)),
+      trailing: TaskCheckBox(isChecked, toggleCheckBox),
     );
+  }
+}
+
+class TaskCheckBox extends StatelessWidget {
+
+  TaskCheckBox(this.checkBoxState, this.toggleCheckBoxState);
+
+  final bool checkBoxState;
+  final Function toggleCheckBoxState;
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(value: checkBoxState,
+    onChanged: toggleCheckBoxState,);
   }
 }
